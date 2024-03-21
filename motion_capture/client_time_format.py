ae3048c79821d0145faae99cb91a1a13f92412c7
Frank_001 <tjzzfrank@gmail.com>
2024-03-22 05:04:00 +0800

    update

--- motion_capture/client_time_format.py
+++ motion_capture/client_time_format.py
@@ -10,7 +10,7 @@ data_list = []
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 local_address = ('localhost', 9763)
 udp_socket.bind(local_address)
-udp_socket.settimeout(10)  
+udp_socket.settimeout(1)  
 
 def time_format(date_str, time_str):
     date_format = "%Y%m%d"
@@ -22,7 +22,7 @@ def time_format(date_str, time_str):
     timestamp = dt.timestamp()
     return timestamp
 
-def parse_euler(date=20240131):
+def parse_euler(date=20240320):
     format_string = '>6sIcblbbbbhh'
     try:
         data, addr = udp_socket.recvfrom(4096) 
@@ -32,15 +32,12 @@ def parse_euler(date=20240131):
         print(timestamp, header[1])
         data_list.append((timestamp, header[1]))
     except socket.timeout:
-        pass  
+        pass
 
-def send_data():
-    file_name = '022.csv'
+if __name__ == "__main__":
+    file_name = 'test_time.csv'
     while not keyboard.is_pressed('esc'):
         parse_euler()
     with open(file_name, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(data_list)
-
-if __name__ == "__main__":
-    send_data()
