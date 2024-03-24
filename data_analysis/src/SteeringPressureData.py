386c4a89d244097adec41177ad6df17d990dc3cb
Frank_001 <tjzzfrank@gmail.com>
2024-03-24 15:09:56 +0800

    update

--- data_analysis/src/SteeringPressureData.py
+++ data_analysis/src/SteeringPressureData.py
@@ -1,6 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class SteeringPressureData:
     def __init__(self, file_path):
@@ -68,3 +73,59 @@ class SteeringPressureData:
 
     def count(self,extracted_data):
         return len(extracted_data)
+    
+
+    def animate_heatmaps(self):
+        """
+        创建一个动画，展示全部数据的热力图，每个热力图为10x96的矩阵。
+        """
+        fig, ax = plt.subplots(figsize=(12, 6))
+
+        def update(frame):
+            ax.clear()
+            pressure_data_str = self.data.iloc[frame]['Pressure_Data']
+            pressure_data = np.fromstring(pressure_data_str, sep=',').reshape((10, 96))
+            c = ax.imshow(pressure_data, cmap='hot', interpolation='nearest')
+            ax.set_title(f"Frame {frame}")
+            return c,
+
+        ani = FuncAnimation(fig, update, frames=len(self.data), interval=100, blit=True,repeat=False)
+        fig.colorbar(ax.images[0], ax=ax, orientation='vertical')
+
+        plt.show()
+        
+    def show_heatmap(self):
+        """
+        使用Tkinter创建一个带有滑动条的GUI。
+        """
+        window = tk.Tk()
+        window.title("Steering Pressure Data")
+        fig, ax = plt.subplots()
+        canvas = FigureCanvasTkAgg(fig, master=window)
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.colorbar = None
+
+        def update_plot(frame):
+            ax.clear()
+            pressure_data_str = self.data.iloc[frame]['Pressure_Data']
+            pressure_data = np.fromstring(pressure_data_str, sep=',').reshape((10, 96))
+            im = ax.imshow(pressure_data, cmap='hot', interpolation='nearest')
+            
+            if not self.colorbar:
+                self.colorbar = fig.colorbar(im, ax=ax)
+            else:
+                self.colorbar.update_normal(im)
+
+            timestamp = self.data.iloc[frame]['timestamp']
+            dt_object = datetime.fromtimestamp(timestamp)
+            formatted_time = dt_object.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+            timestamp_label.config(text=f"Timestamp: {formatted_time}")
+            canvas.draw()
+        timestamp_label = tk.Label(window, text="Time: ", font=("Arial", 12))
+        timestamp_label.pack(side=tk.TOP, fill=tk.X, expand=0)
+        slider = ttk.Scale(window, from_=0, to=len(self.data)-1, orient=tk.HORIZONTAL, command=lambda s: update_plot(int(float(s))))
+        slider.pack(side=tk.BOTTOM, fill=tk.X, expand=1)
+        update_plot(0)
+        
+        window.mainloop()
