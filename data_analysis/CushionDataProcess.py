c300538012b22a813dfe813c982ce70ffced77af
Franky <z_franky@tongji.edu.cn>
2024-04-01 17:13:10 +0800

    added cushion data process script

---
+++ data_analysis/CushionDataProcess.py
@@ -0,0 +1,21 @@
+from src.TaskManager import TaskManager
+from src.CushionData import CushionData
+import pandas as pd
+
+cushion_data_file_path = './test_data/cushion_test.csv'
+save_file_path = './test_data/cushion_cal.csv'
+radio =  7.50062
+threshold_kpm2 = 0.43 
+unit = 1.0
+
+if __name__ == "__main__":
+    cushion = CushionData(cushion_data_file_path)
+    cushion.count_pressure_over_threshold(threshold_mmHg=threshold_kpm2*radio,unit=unit)
+    cushion.calculate_pressure_sum_change()
+    cushion.calculate_centroid_movement()
+    print(cushion.data.head())
+    
+    
+    
+    
+    cushion.save_data(save_file_path)
