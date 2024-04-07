6c181f0ae1088383480b3fc1fd94275732b3632b
Franky <z_franky@tongji.edu.cn>
2024-04-07 13:31:12 +0800

    update

---
+++ cushion_pressure/SDK/FileTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AxisBridgeConsoleDemo
+{
+    class FileTest
+    {
+        static unsafe void Main(string[] args)
+        {
+            try
+            {
+                // 读取文本文件
+                using (StreamReader sr = new StreamReader("config.txt"))
+                {
+                    string line;
+                    // ReadLine()一行一行的循环读取
+                    //当然可以直接ReadToEnd()读到最后
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    Thread.Sleep(10000);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+
+}
--- cushion_pressure/bin/Debug/releaseRun64b.bat
+++
@@ -1,3 +0,0 @@
-del /F AxisBridge.dll
-copy D:\home\working\build-AxisBridge-Desktop_Qt_5_15_2_MSVC2019_64bit-Release\release\AxisBridge.dll .
-AxisBridgeConsoleDemo.exe
